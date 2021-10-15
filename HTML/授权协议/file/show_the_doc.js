f360119f876c5af88d57cc60d2ead13ce5b470dc
AdamSmith <sogaxili@gmail.com>
2021-10-15 15:02:12 +0800

    feat: 添加授权协议和IE适配代码

---
+++ HTML/授权协议/file/show_the_doc.js
@@ -0,0 +1,20 @@
+﻿
+var read_the_doc = {};
+
+//word 
+read_the_doc.word = function (parm,content_id) {
+
+    var options = {
+        height: "1080px",
+        pdfOpenParams: { view: 'FitV', page: '0' },
+        name: "mans",
+        fallbackLink: "<p>您的浏览器暂不支持此pdf，请下载最新的浏览器</p>"
+    };
+
+    PDFObject.embed(parm, content_id, options);
+
+}
+
+
+
+
