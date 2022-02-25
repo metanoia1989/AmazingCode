496da06c333883322e64a4ba079f7073aaeba298
Adam Smith <sogaxili@gmail.com>
2022-02-25 10:27:30 +0800

    feat: 获取当前请求完整URL

--- PHP/functions.php
+++ PHP/functions.php
@@ -719,3 +719,26 @@ function encodeURIComponent($str) {
     $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
     return strtr(rawurlencode($str), $revert);
 }
+
+
+
+/**
+ * 获取当前页面完整URL
+ * @doc https://www.cnblogs.com/xuzhengzong/p/6856367.html
+ * 
+ * @return void
+ */
+function get_current_url()
+{
+    $current_url='http://';
+    if(isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']=='on'){
+        $current_url='https://';
+    }
+    if($_SERVER['SERVER_PORT']!='80'){
+        $current_url.=$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
+    }else{
+        $current_url.=$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+    }
+
+    return $current_url;
+}
