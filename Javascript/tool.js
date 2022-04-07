6aae5f3d90e9c0032f780d35be77801c9c737334
Adam Smith <sogaxili@gmail.com>
2022-04-07 10:10:43 +0800

    feat: 添加空对象检测函数

--- Javascript/tool.js
+++ Javascript/tool.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: AdamSmith
+ * @Date: 2022-01-22 10:39:20
+ * @LastEditors: AdamSmith
+ * @LastEditTime: 2022-04-07 10:02:36
+ * @Description: 
+ * email: sogaxili@gmail.com
+ * Copyright (c) 2022 by AdamSmith, All Rights Reserved. 
+ */
 /**
  * 给a标签字符串添加 _blank 
  * 
@@ -12,4 +21,21 @@ function addBlankTargets(s) {
   var p = $('<p>' + s + '</p>');
   p.find('a').attr('target', '_blank');
   return p.html();
+}
+
+function isEmptyObj(obj) {
+  return obj // 👈 null and undefined check
+    && Object.keys(obj).length === 0
+    && Object.getPrototypeOf(obj) === Object.prototype
+}
+
+// Pre-ECMA 5: 
+function isEmpty(obj) {
+  for(var prop in obj) {
+    if(Object.prototype.hasOwnProperty.call(obj, prop)) {
+      return false;
+    }
+  }
+
+  return JSON.stringify(obj) === JSON.stringify({});
 }
